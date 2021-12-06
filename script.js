f026a658f3f6e4195b49dc2ed0558948e43e8ff8
fabianobarroli <fabianobarroli@gmail.com>
2021-12-06 14:39:13 -0300

    criando função

---
+++ script.js
@@ -0,0 +1,26 @@
+function cesar(){
+   var cesar = document.querySelector("cifra-cesar")
+   var novoHtml = document.createElement("html")
+   cesar.appendChild(novoHtml)
+   novoHtml.innerHTML = <input type="range" value="10" min="1" max="26" oninput="this.nextElementSibling.value = this.value">;
+   <output>10</output>
+}
+
+function base(){
+
+}
+
+function codificar(){
+
+
+}
+
+function decodificar(){
+
+}
+
+function botao(){
+
+    
+}
+
